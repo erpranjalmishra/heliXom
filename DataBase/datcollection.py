7f4a3c19de3b1b7682d2662758e0c7d4a8c940f5
Pranjal Mishra <x@pranjalmishra.dev>
2024-09-05 02:36:36 +0530

    update

--- DataBase/datcollection.py
+++ DataBase/datcollection.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 offset = 20
 imgSize = 300
-folder = "Data/X"
+folder = "Data/U"
 counter = 0
 while True:
     success, img = cap.read()
--- DataBase/testing.py
+++ DataBase/testing.py
@@ -10,8 +10,7 @@ classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
 offset = 20
 imgSize = 300
 counter = 0
-labels = ["A", "B", "C"]
-
+labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z','X']
 while True:
     success, img = cap.read()
     imgOutput = img.copy()
